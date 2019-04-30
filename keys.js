b91b4450445ed9a2666512dd55de7b4fa1199962
ramonavidallon <ramon.vidallon@gmail.com>
2019-04-30 15:29:57 -0700

    api key additions

--- keys.js
+++ keys.js
@@ -2,5 +2,14 @@ console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  secret: process.env.SPOTIFY_SECRET,
 };
+
+exports.omdb = {
+  id: process.env.OMDB_ID,
+  secret: process.env.OMDB_SECRET,
+}
+
+exports.bandsintown = {
+  id: process.env.BANDSINTOWN_ID,
+}

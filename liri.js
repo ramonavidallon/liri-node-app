65e8d23d3c2d2be5aa24664a3afeeaa7a6cdf5f8
ramonavidallon <ramon.vidallon@gmail.com>
2019-05-03 00:12:25 -0700

    concertThis function created and working minus date of event

--- liri.js
+++ liri.js
@@ -13,6 +13,7 @@ var searchType = process.argv.splice(3).join();
 
 
 
+
 //TODO: grab user input, grab parameters from user input, make API calls 
 
 
@@ -29,7 +30,7 @@ else if (input === 'spotify-this-song') {
 } 
 //BANDS IN  TOWN//
 else if (input === 'concert-this') {
-  console.log("concert chosen!");
+  concertThis(searchType);
 }
 else if (input === 'do-what-it-says') {
   console.log("do what it says");
@@ -40,17 +41,13 @@ else {
 /////END IF ELSE IF STATMENTS//////
 
 
-//OMDB FUNCTION//
+//START OMDB FUNCTION//
 function movieThis(movie) {
 
-  var movieSearch = movie
   var movieQuery = movie || "Mr. Nobody"
   
   axios.get("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(function(response) {
     
-    for (var i = 3; i < movieSearch.length; i++) {
-      reference.push(movieSearch[i])
-    }
     var divider = "\n------------------------------------------------------------\n\n";
     var jsonData = response.data;
 
@@ -78,10 +75,47 @@ function movieThis(movie) {
   })
 
 };
-////////////END OMDOB FUNCTION//////////
+/////END OMDB FUNCTION/////
 
 
-// //SPOTIFY FUNCTION//
+/////START BANDS IN TOWN FUNCTION/////
+function concertThis(concert) {
+
+  var concertQuery = concert || "'The Sign' by Ace of Base"
+
+  axios.get("https://rest.bandsintown.com/artists/" + concertQuery + "/events?app_id=codingbootcamp").then(function(response) {
+    var jsonData = response.data;
+    console.log(jsonData);
+    for (var i = 0; i < jsonData.length; i++) {
+      var divider = "\n------------------------------------------------------------\n\n";
+      var concertFind = [
+        "\nVenue Name: " + jsonData[i].venue.name,
+        "\nLocation: " + jsonData[i].venue.city,
+        "\nDate of Concert: " + moment(jsonData[0], "YYYY-DD-MM").format("DD/MM/YYYY"),
+      ].join("\n\n")
+
+      console.log(divider + concertFind);
+      console.log(concertThis);
+    };
+      
+  });
+};   
+/////END BANDS IN TOWN FUNCTION/////
+
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+/////SPOTIFY FUNCTION/////
 
 // function spotifyTrack(value) {
 //   if(!value) {
@@ -108,7 +142,4 @@ function movieThis(movie) {
 //     console.log(err);
 //   });
 // }
-
-
-
 
